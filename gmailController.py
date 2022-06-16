7b4f4b93d3011554c615862a399a2616e80a5e36
Ratchapol <ratcahpol.c@ku.th>
2022-06-16 11:26:37 +0700

    add ability to attach file both normal attach and inline

--- gmailController.py
+++ gmailController.py
@@ -15,16 +15,13 @@ from googleapiclient.discovery import build, Resource
 from googleapiclient.errors import HttpError
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
-from typing import Dict, List, Union
+from typing import Dict, List, Tuple, Union
 
-# | GLOBAL VARIABLES
-# CREDENTIALS_FILENAME = "credentials.json"
-# MAIL_LIST_FILENAME = "staff_res.csv"
-# RESULT_FILENAME = "result.csv"
-SCOPES = ["https://mail.google.com/"]
 
 # | FUNCTIONS
-def create_service(CREDENTIALS_FILENAME, SCOPES = ["https://mail.google.com/"]) -> Union[Resource, None]:
+def create_service(
+    CREDENTIALS_FILENAME, SCOPES=["https://mail.google.com/"]
+) -> Union[Resource, None]:
     creds = None
 
     if not creds or not creds.valid:
@@ -46,47 +43,8 @@ def create_service(CREDENTIALS_FILENAME, SCOPES = ["https://mail.google.com/"]) 
         return None
 
 
-# def create_message_with_files(
-#     receiver: str, subject: str, text: str, files: List[str]
-# ) -> Dict[str, str]:
-#     message = MIMEMultipart()
-#     message["to"] = receiver
-#     message["subject"] = subject
-
-#     msg = MIMEText(text, "html")
-#     message.attach(msg)
-
-#     _id = 1
-#     for file, mode in files:
-#         if file is not None:
-#             content_type, encoding = mimetypes.guess_type(file)
-#             if content_type is None or encoding is not None:
-#                 content_type = "application/octet-stream"
-#             main_type, sub_type = content_type.split("/", 1)
-#             if main_type == "image":
-#                 with open(file, "rb") as fp:
-#                     msg = MIMEImage(fp.read(), _subtype=sub_type)
-#             else:
-#                 with open(file, "rb") as fp:
-#                     msg = MIMEBase(main_type, sub_type)
-#                     msg.set_payload(fp.read())
-
-#             filename = os.path.basename(file)
-#             if mode == "inline":
-#                 msg.add_header("Content-Id", f"<file{_id}>")
-#                 msg.add_header("Content-Disposition", "inline", filename=filename)
-#                 _id += 1
-#             else:
-#                 msg.add_header("Content-Disposition", "attachment", filename=filename)
-#                 if sub_type == "pdf":
-#                     encoder.encode_base64(msg)
-#             message.attach(msg)
-
-#     return {"raw": base64.urlsafe_b64encode(message.as_bytes()).decode()}
-
-
-def create_message(
-    receiver: str, subject: str, text: str
+def create_message_with_files(
+    receiver: str, subject: str, text: str, files: List[Tuple[str]]
 ) -> Dict[str, str]:
     message = MIMEMultipart()
     message["to"] = receiver
@@ -95,7 +53,47 @@ def create_message(
     msg = MIMEText(text, "html")
     message.attach(msg)
 
+    _id = 1
+    for file, mode in files:
+        if file is not None:
+            content_type, encoding = mimetypes.guess_type(file)
+            if content_type is None or encoding is not None:
+                content_type = "application/octet-stream"
+            main_type, sub_type = content_type.split("/", 1)
+
+            filename = os.path.basename(file)
+
+            if main_type == "image":
+                with open(file, "rb") as fp:
+                    msg = MIMEImage(fp.read(), _subtype=sub_type, name=filename)
+            else:
+                with open(file, "rb") as fp:
+                    msg = MIMEBase(main_type, sub_type)
+                    msg.set_payload(fp.read())
+
+            if mode == "inline":
+                msg.add_header("Content-Id", f"<file{_id}>")
+                msg.add_header("Content-Disposition", "inline", filename=filename)
+                _id += 1
+            else:
+                msg.add_header("Content-Disposition", "attachment", filename=filename)
+                if sub_type == "pdf":
+                    encoder.encode_base64(msg)
+            message.attach(msg)
+
     return {"raw": base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+
+def create_message(receiver: str, subject: str, text: str) -> Dict[str, str]:
+    message = MIMEMultipart()
+    message["to"] = receiver
+    message["subject"] = subject
+
+    msg = MIMEText(text, "html")
+    message.attach(msg)
+
+    return {"raw": base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
 
 def send_msg(
     service: Resource, user_id: str, message: Dict[str, str]
@@ -106,13 +104,3 @@ def send_msg(
     except HttpError as error:
         print(f"Error\n\n{error}")
         return None
-
-# # | MAIN
-# if __name__ == "__main__":
-#     service = create_service()
-#     parsed_html = "" # html that already parsed and ready to send
-#     subject = "" # subject
-#     email = "" # receiver e-mail
-#     content = create_message(email, subject, parsed_html)
-#     res = send_msg(service, "me", content)
-#     print(res)
